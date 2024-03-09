f0023fcf07f1d730e2f199f10086f81b4b8a4811
TommasoPrinetti <Tommi.prinetti@gmail.com>
2024-03-09 17:45:14 +0100

    Updated lib path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
-    import '/Users/tommasoprinetti/Documents/PORTFOLIO_FOTO_SVELTE/PhotoPortfolio/src/lib/styles/styles.css';
+    import '$lib/styles/styles.css';
     import { dev } from '$app/environment';
 </script>
 
-<slot />
+<slot /> 
