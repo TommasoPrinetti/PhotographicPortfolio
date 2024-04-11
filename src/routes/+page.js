ea4e7e19438e31b8e05e62844a3e594b2ba7cb21
TommasoPrinetti <Tommi.prinetti@gmail.com>
2024-04-11 14:41:27 +0200

    Added MUD

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -58,6 +58,11 @@ let capitoliFoto = [
       location: "Milano - Piazza del Duomo",
       descrizione: "La casa/studio di Carol Rama si colloca a Torino in un ambiente completamente oscuro e al riparo da ogni tipo di luce. Il lavoro d'indagine fotografica ha voluto prioritizzare l'intrusione della poca luce (artificiale) nella casa, e del suo rapporto con gli oggetti che assumono forme e silhouette appena percettibili.",
    },
+   {
+      titolo: "MilanoUniversityDistrict",
+      location: "Milano - Piazza Leonardo",
+      descrizione: "La casa/studio di Carol Rama si colloca a Torino in un ambiente completamente oscuro e al riparo da ogni tipo di luce. Il lavoro d'indagine fotografica ha voluto prioritizzare l'intrusione della poca luce (artificiale) nella casa, e del suo rapporto con gli oggetti che assumono forme e silhouette appena percettibili.",
+   },
     {
        titolo: "Curriculum vitae",
        location: "",
